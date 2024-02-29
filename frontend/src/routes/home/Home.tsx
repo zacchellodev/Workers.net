ad138778a5de610c4a3b6b0b890ac5aebfb38c8f
Carlos Zacchello <carloszacchello@MacKadu.local>
2024-02-28 22:07:02 -0300

    add: por tras da equipe, why us TODO: como comecar

--- frontend/src/routes/home/Home.tsx
+++ frontend/src/routes/home/Home.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import GabrielPedrosoPfp from './GabrielPedrosoPfp.jpg'
 import './Home.css'
 
 export default function Home() {
@@ -25,9 +26,31 @@ export default function Home() {
         <a href="#">ABOUT US</a>
       </div>
       <div className="home-main">
-        <div className="left">
-          <h1>Porque a <span>Workers.net</span>?</h1>
-        </div>
+        <article className="left">
+          <div className="whyUs">
+            <h1>Porque a <span>Workers.net</span>?</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.<br />
+            Atque quaerat voluptatibus quas enim dicta,<br />
+            aliquam error impedit velit quasi ratione dolores, alias nihil,<br />
+            labore quibusdam. Cumque officiis modi at neque.</p>
+          </div>
+          <div className="howStart">
+            <h1>Como começar</h1>
+          </div>
+        </article>
+        <article className="right">
+          <h1 id="title">Por trás da equipe,<br />quem somos nós?</h1>
+          <div className="pic-name-function">
+            <a href="https://www.zacchello.site/" target="_blank"><h1>João Zacchello</h1></a>
+            <img src="https://avatars.githubusercontent.com/u/144489119?v=4" alt="Joao Zacchello Github Picture" />
+            <h2>Fundador e CEO da<br />Workers.net</h2>
+          </div>
+          <div className="pic-name-function">
+            <a href="#" target="_blank"><h1>Gabriel Pedroso</h1></a>
+            <img src={GabrielPedrosoPfp} alt="Gabriel Pedroso Instagram Picture" />
+            <h2>Co-Fundador da<br /> Workers.net</h2>
+          </div>
+        </article>
       </div>
     </div>
   )
